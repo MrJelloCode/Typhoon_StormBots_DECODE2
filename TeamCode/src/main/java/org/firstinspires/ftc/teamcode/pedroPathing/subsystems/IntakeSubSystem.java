f4811a79a86e34a3374878b11d2bb682098bab25
Malaravan Vijayakumar <MalaravanV5@gmail.com>
2025-10-19 16:09:40 -0400

    Telemetery data has been added to Intake Sub System

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/subsystems/IntakeSubSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/subsystems/IntakeSubSystem.java
@@ -12,14 +12,15 @@ public class IntakeSubSystem {
     private DcMotorEx intakeMotor;
     private PIDFController pidf;
 
-    // PIDF constants (tunable from dashboard)
+    // PIDF coefficients (tunable from dashboard)
     public static double P = 0.02;
     public static double I = 0.0;
     public static double D = 0.0;
     public static double F = 0.16;
 
-    // Default velocity (can be overridden in opmode)
     private double targetVelocity = 0;
+    private double currentVelocity = 0;
+    private double error = 0;
 
     public IntakeSubSystem(HardwareMap hardwareMap) {
         intakeMotor = hardwareMap.get(DcMotorEx.class, "Intake");
@@ -27,33 +28,51 @@ public class IntakeSubSystem {
         intakeMotor.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
 
         pidf = new PIDFController(P, I, D, F);
-        pidf.setTolerance(100, 200);
+        pidf.setTolerance(100, 200); // error and derivative tolerance
     }
 
-    // --- NEW ---
+    // Allow OpMode to set velocity dynamically
     public void setTargetVelocity(double velocity) {
         this.targetVelocity = velocity;
         pidf.setSetPoint(velocity);
     }
 
+    // Called periodically (each loop)
     public void update() {
-        // Refresh PIDF values
+        // Update PIDF constants live from dashboard
         pidf.setPIDF(P, I, D, F);
+        pidf.setSetPoint(targetVelocity);
 
-        // Read current motor velocity
-        double currentVelocity = intakeMotor.getVelocity();
+        // Read encoder velocity (ticks per second)
+        currentVelocity = intakeMotor.getVelocity();
 
-        // Calculate new motor output
+        // Compute output power
         double output = pidf.calculate(currentVelocity);
 
-        // Clamp between -1 and 1
+        // Save error for telemetry
+        error = pidf.getPositionError();
+
+        // Clamp to [-1, 1] for safety
         output = Math.max(-1.0, Math.min(1.0, output));
 
+        // Apply motor power
         intakeMotor.setPower(output);
     }
 
     public boolean atTargetVelocity() {
         return pidf.atSetPoint();
+    }
+
+    public double getCurrentVelocity() {
+        return currentVelocity;
+    }
+
+    public double getTargetVelocity() {
+        return targetVelocity;
+    }
+
+    public double getError() {
+        return error;
     }
 
     public void stop() {
