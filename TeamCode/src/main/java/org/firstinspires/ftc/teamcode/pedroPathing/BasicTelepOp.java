8c82cd59a2440dc1d962e7b48aad46a81d47b859
Malaravan Vijayakumar <MalaravanV5@gmail.com>
2026-04-27 14:26:11 -0400

    Working H-DRIVE
    Signed-off-by: Malaravan Vijayakumar <MalaravanV5@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/BasicTelepOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/BasicTelepOp.java
@@ -3,19 +3,37 @@ package org.firstinspires.ftc.teamcode.pedroPathing;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
 public class BasicTelepOp extends LinearOpMode {
-    private DcMotorEx leftMotor, rightMotor;
+    private DcMotorEx leftMotor, centerMotor, rightMotor;
     @Override
     public void runOpMode() throws InterruptedException {
 
         leftMotor = hardwareMap.get(DcMotorEx.class,"leftMotor");
         rightMotor = hardwareMap.get(DcMotorEx.class,"rightMotor");
+        centerMotor= hardwareMap.get(DcMotorEx.class,"centerMotor");
+
+        rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
         waitForStart();
+
         while(opModeIsActive()){
-            leftMotor.setPower(-gamepad1.right_stick_y);
-            rightMotor.setPower(-gamepad1.right_stick_y);
+
+            double speed = -gamepad1.left_stick_y;
+            double turn = gamepad1.right_stick_x;
+
+            double left = speed + turn;
+            double right = speed - turn;
+
+            rightMotor.setPower(right);
+            leftMotor.setPower(left);
+            centerMotor.setPower(gamepad1.left_stick_x);
+
+
+
         }
     }
 }
